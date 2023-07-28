0b5954f9d76097578e12ff1029299636dc4cfc94
Illia Charushyn <charushyngg@gmail.com>
2023-07-28 22:04:20 +0300

    start
    налаштовував фонтс + розбирався в тайлвінді, ноут на зарядку((

---
+++ src/app/components/Main.js
@@ -0,0 +1,9 @@
+import Header from "./Header"
+
+export default function Main(){
+    return(
+        <div class='flex-col px-20 relative h-screen w-full bg-main'>
+            <Header class='z-10'></Header>
+        </div>
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'main': "url('/main.jpg')",
+      },
+      colors: {
+        white: '#F5F5F5',
+        gold: '#CCBC67',
+        bgblack: '#222221',
+        light_gold: '#E5DEBB',
+        gray: '#525252',
+      },
+      fontFamily: {
+        Acrom_light: 'Acrom_light',
+        Acrom_regular: 'Acrom_regular',
+        Acrom_bold: 'Acrom_bold'
+      }
+    },
+  },
+  plugins: [],
+}
